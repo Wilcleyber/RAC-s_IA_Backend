c735363eedacec4972e3f1a25d521c8253db4dab
unknown <wilcleyber2@gmail.com>
2026-03-21 05:11:12 -0300

    adicionado Vercel ao cors!

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-# Configuração de CORS (Essencial para o Frontend conversar com o Render)
+origins = [
+    "http://localhost:5173",
+    "https://rac-ia.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
